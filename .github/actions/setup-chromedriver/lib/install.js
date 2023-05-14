7acc7feb5364d330a12d1c7a2d5ca5e6ee6acd40
Kevin Ramage <kevinramage@hotmail.fr>
2023-05-14 13:36:23 +0200

    Add setup chrome driver action

---
+++ .github/actions/setup-chromedriver/lib/install.js
@@ -0,0 +1,174 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Install = void 0;
+const tc = __importStar(require("@actions/tool-cache"));
+const exec = __importStar(require("@actions/exec"));
+const core = __importStar(require("@actions/core"));
+const fs_1 = __importDefault(require("fs"));
+const plateform_1 = require("./plateform");
+class Install {
+    getDownloadUrl(version, plateform) {
+        if (plateform.getSystem() == plateform_1.SYSTEM_TYPE.WINDOWS) {
+            if (plateform.getArchitecture() == plateform_1.ARCHITECTURE_TYPE.I686) {
+                return `https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Win%2F${version}%2Fchrome-win.zip?alt=media`;
+            }
+            else if (plateform.getArchitecture() == plateform_1.ARCHITECTURE_TYPE.AMD64) {
+                return `https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Win_x64%2F${version}%2Fchrome-win.zip?alt=media`;
+            }
+        }
+        else if (plateform.getSystem() == plateform_1.SYSTEM_TYPE.LINUX) {
+            if (plateform.getArchitecture() == plateform_1.ARCHITECTURE_TYPE.I686) {
+                return `https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux%2F${version}%2Fchrome-linux.zip?alt=media`;
+            }
+            else if (plateform.getArchitecture() == plateform_1.ARCHITECTURE_TYPE.AMD64) {
+                return `https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F${version}%2Fchrome-linux.zip?alt=media`;
+            }
+        }
+        else if (plateform.getSystem() == plateform_1.SYSTEM_TYPE.DARWIN) {
+            if (plateform.getArchitecture() == plateform_1.ARCHITECTURE_TYPE.I686) {
+                return `https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F${version}%2Fchrome-mac.zip?alt=media`;
+            }
+            else if (plateform.getArchitecture() == plateform_1.ARCHITECTURE_TYPE.AMD64) {
+                return `https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F${version}%2Fchrome-mac.zip?alt=media`;
+            }
+        }
+        throw `Unsupported plateform: ${plateform.getSystem()} - ${plateform.getArchitecture()}`;
+    }
+    downloadSetup(version, plateform) {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            core.info(`Download setup for version ${version} and plateform ${plateform.getSystem()} - ${plateform.getArchitecture()}`);
+            const url = this.getDownloadUrl(version, plateform);
+            try {
+                const archivePath = yield tc.downloadTool(url);
+                resolve(archivePath);
+            }
+            catch (err) {
+                core.error(`An error during download setup: ${err}`);
+                reject(err);
+            }
+        }));
+    }
+    installUnix(archivePath) {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            core.info(`Install to unix system: ${archivePath}`);
+            try {
+                // Unarchive
+                yield exec.exec("unzip", ["-d", "/opt/chrome", archivePath]);
+                // Remove archive
+                yield fs_1.default.promises.unlink(archivePath);
+                // Add chrome to path
+                core.info(`Add chrome binary to path`);
+                //await exec.exec("ln", ["-s", "/opt/chrome/chrome-linux/chrome", "chrome"]);
+                core.addPath("/opt/chrome/chrome-linux");
+                resolve();
+            }
+            catch (err) {
+                core.error(`An error occured during installation: ${err}`);
+                reject(err);
+            }
+        }));
+    }
+    installDarwin(archivePath) {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            core.info(`Install to darwin system: ${archivePath}`);
+            try {
+                // Unarchive
+                yield exec.exec("unzip", ["-d", "/opt/chrome", archivePath]);
+                // Remove archive
+                yield fs_1.default.promises.unlink(archivePath);
+                // Add chrome to path
+                core.info(`Add chrome binary to path`);
+                //await exec.exec("ln", ["-s", "/opt/chrome/chrome-linux/chrome", "chrome"]);
+                core.addPath("/opt/chrome/chrome-mac");
+                resolve();
+            }
+            catch (err) {
+                core.error(`An error occured during installation: ${err}`);
+                reject(err);
+            }
+        }));
+    }
+    installWindows(archivePath, plateform) {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            core.info(`Install to windows system: ${archivePath}`);
+            try {
+                // Unarchive
+                const destination = plateform.getArchitecture() == plateform_1.ARCHITECTURE_TYPE.AMD64 ? "C:\\Program Files" : "C:\\Program Files (x86)";
+                yield exec.exec("7z", ["x", archivePath, `-o${destination}`]);
+                // Remove archive
+                yield fs_1.default.promises.unlink(archivePath);
+                // Add chrome to path
+                core.info(`Add chrome binary to path`);
+                //await exec.exec("ln", ["-s", "/opt/chrome/chrome-linux/chrome", "chrome"]);
+                core.addPath(`{destination}\\chrome-win`);
+                resolve();
+            }
+            catch (err) {
+                core.error(`An error occured during installation: ${err}`);
+                reject(err);
+            }
+        }));
+    }
+    install(version, plateform) {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            core.info(`Install version ${version} for plateform ${plateform.getSystem()} - ${plateform.getArchitecture()}`);
+            try {
+                // Download version
+                const archivePath = yield this.downloadSetup(version, plateform);
+                // Install binary (Unix)
+                if (plateform.getSystem() == plateform_1.SYSTEM_TYPE.LINUX) {
+                    yield this.installUnix(archivePath);
+                    // Install binary (Mac)
+                }
+                else if (plateform.getArchitecture() == plateform_1.SYSTEM_TYPE.DARWIN) {
+                    yield this.installDarwin(archivePath);
+                    // Install binary (Windows)
+                }
+                else if (plateform.getSystem() == plateform_1.SYSTEM_TYPE.WINDOWS) {
+                    yield this.installWindows(archivePath, plateform);
+                }
+                resolve();
+            }
+            catch (err) {
+                core.error(String(err));
+                reject(err);
+            }
+        }));
+    }
+}
+exports.Install = Install;
---
+++ .github/actions/setup-chromedriver/src/install.ts
@@ -0,0 +1,146 @@
+import * as tc from "@actions/tool-cache";
+import * as exec from "@actions/exec";
+import * as core from "@actions/core";
+import os, { version } from "os";
+import fs from "fs";
+import { ARCHITECTURE_TYPE, Plateform, SYSTEM_TYPE } from "./plateform";
+
+export class Install {
+
+    private getDownloadUrl(version: string, plateform: Plateform) : string {
+        if (plateform.getSystem() == SYSTEM_TYPE.WINDOWS) {
+            if (plateform.getArchitecture() == ARCHITECTURE_TYPE.I686) {
+                return `https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Win%2F${version}%2Fchromedriver-win.zip?alt=media`;
+            } else if (plateform.getArchitecture() == ARCHITECTURE_TYPE.AMD64) {
+                return `https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Win_x64%2F${version}%2Fchromedriver-win.zip?alt=media`;
+            }
+        } else if (plateform.getSystem() == SYSTEM_TYPE.LINUX) {
+            if (plateform.getArchitecture() == ARCHITECTURE_TYPE.I686) {
+                return `https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux%2F${version}%2Fchromedriver-linux.zip?alt=media`;
+            } else if (plateform.getArchitecture() == ARCHITECTURE_TYPE.AMD64) {
+                return `https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F${version}%2Fchromedriver-linux.zip?alt=media`;
+            }
+        } else if (plateform.getSystem() == SYSTEM_TYPE.DARWIN) {
+            if (plateform.getArchitecture() == ARCHITECTURE_TYPE.I686) {
+                return `https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F${version}%2Fchromedriver-mac.zip?alt=media`;
+            } else if (plateform.getArchitecture() == ARCHITECTURE_TYPE.AMD64) {
+                return `https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F${version}%2Fchromedriver-mac.zip?alt=media`;
+            }
+        }
+        throw `Unsupported plateform: ${plateform.getSystem()} - ${plateform.getArchitecture()}`;
+    }
+
+    private downloadSetup(version: string, plateform : Plateform) {
+        return new Promise<string>(async(resolve, reject) => {
+            core.info(`Download setup for version ${version} and plateform ${plateform.getSystem()} - ${plateform.getArchitecture()}`)
+            const url = this.getDownloadUrl(version, plateform);
+            try {
+                const archivePath = await tc.downloadTool(url);
+                resolve(archivePath);
+            } catch (err) {
+                core.error(`An error during download setup: ${err}`)
+                reject(err);
+            }
+        })
+    }
+
+    private installUnix(archivePath: string) {
+        return new Promise<void>(async (resolve, reject) => {
+            core.info(`Install to unix system: ${archivePath}`);
+
+            try {
+                // Unarchive
+                await exec.exec("unzip", ["-d", "/opt/chrome", archivePath])
+
+                // Remove archive
+                await fs.promises.unlink(archivePath);
+
+                // Add chrome to path
+                core.info(`Add chrome binary to path`);
+                //await exec.exec("ln", ["-s", "/opt/chrome/chrome-linux/chrome", "chrome"]);
+                core.addPath("/opt/chrome/chromedriver-linux")
+                resolve();
+
+            } catch (err) {
+                core.error(`An error occured during installation: ${err}`);
+                reject(err);
+            }
+        });
+    }
+
+    private installDarwin(archivePath: string) {
+        return new Promise<void>(async (resolve, reject) => {
+            core.info(`Install to darwin system: ${archivePath}`);
+
+            try {
+                // Unarchive
+                await exec.exec("unzip", ["-d", "/opt/chrome", archivePath])
+
+                // Remove archive
+                await fs.promises.unlink(archivePath);
+
+                // Add chrome to path
+                core.info(`Add chrome binary to path`);
+                //await exec.exec("ln", ["-s", "/opt/chrome/chrome-linux/chrome", "chrome"]);
+                core.addPath("/opt/chrome/chromedriver-mac")
+                resolve();
+
+            } catch (err) {
+                core.error(`An error occured during installation: ${err}`);
+                reject(err);
+            }
+        });
+    }
+
+    private installWindows(archivePath: string, plateform: Plateform) {
+        return new Promise<void>(async (resolve, reject) => {
+            core.info(`Install to windows system: ${archivePath}`);
+
+            try {
+                // Unarchive
+                const destination = plateform.getArchitecture() == ARCHITECTURE_TYPE.AMD64 ? "C:\\Program Files" : "C:\\Program Files (x86)"
+                await exec.exec("7z", ["x", archivePath, `-o${destination}`])
+
+                // Remove archive
+                await fs.promises.unlink(archivePath);
+
+                // Add chrome to path
+                core.info(`Add chrome binary to path`);
+                //await exec.exec("ln", ["-s", "/opt/chrome/chrome-linux/chrome", "chrome"]);
+                core.addPath(`{destination}\\chromedriver-win`)
+                resolve();
+
+            } catch (err) {
+                core.error(`An error occured during installation: ${err}`);
+                reject(err);
+            }
+        });
+    }
+
+    public install(version: string, plateform: Plateform) {
+        return new Promise<void>(async (resolve, reject) => {
+            core.info(`Install version ${version} for plateform ${plateform.getSystem()} - ${plateform.getArchitecture()}`)
+
+            try {
+                // Download version
+                const archivePath = await this.downloadSetup(version, plateform);
+
+                // Install binary (Unix)
+                if (plateform.getSystem() == SYSTEM_TYPE.LINUX) {
+                    await this.installUnix(archivePath);
+                // Install binary (Mac)
+                } else if (plateform.getArchitecture() == SYSTEM_TYPE.DARWIN) {
+                    await this.installDarwin(archivePath);
+                // Install binary (Windows)
+                } else if (plateform.getSystem() == SYSTEM_TYPE.WINDOWS) {
+                    await this.installWindows(archivePath, plateform);
+                }
+
+                resolve();
+            } catch (err) {
+                core.error(String(err));
+                reject(err);
+            }
+        });
+    }
+}
