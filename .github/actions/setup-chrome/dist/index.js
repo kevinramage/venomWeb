bd6b20fc6bd0c8290949c2b6f0a7f440109e60c1
Kevin Ramage <kevinramage@hotmail.fr>
2023-05-17 17:51:32 +0200

    Add revision for darwin system

--- .github/actions/setup-chrome/dist/index.js
+++ .github/actions/setup-chrome/dist/index.js
@@ -7174,12 +7174,15 @@ https://www.chromium.org/getting-involved/download-chromium/
 https://chromiumdash.appspot.com/branches
 http://cros-omahaproxy.appspot.com/
 
-112 => 1109224 => 1109220
+112 => 1109224 => 1109220 (Unix), 1109213 (Mac)
 https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Linux_x64/1109220/
-113 => 1121455 => 1121454
+https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Mac/1109213/
+113 => 1121455 => 1121454 (Unix), 1121458 (Mac)
 https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Linux_x64/1121454/
-114 => 1135570 => 1135561
+https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Mac/1121448/
+114 => 1135570 => 1135561 (Unix), 1135562 (Mac)
 https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Linux_x64/1135561/
+https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Mac/1135562/
 */
 class Index {
     run() {
@@ -7194,13 +7197,28 @@ class Index {
                     // Identify to download
                     let versionToDownload = "";
                     if (version == "112") {
-                        versionToDownload = "1109220";
+                        if (plateform.getSystem() == plateform_1.SYSTEM_TYPE.DARWIN) {
+                            versionToDownload = "1109213";
+                        }
+                        else if (plateform.getSystem() == plateform_1.SYSTEM_TYPE.LINUX) {
+                            versionToDownload = "1109220";
+                        }
                     }
                     else if (version == "113") {
-                        versionToDownload = "1121454";
+                        if (plateform.getSystem() == plateform_1.SYSTEM_TYPE.DARWIN) {
+                            versionToDownload = "1121458";
+                        }
+                        else if (plateform.getSystem() == plateform_1.SYSTEM_TYPE.LINUX) {
+                            versionToDownload = "1121454";
+                        }
                     }
                     else if (version == "114") {
-                        versionToDownload = "1135561";
+                        if (plateform.getSystem() == plateform_1.SYSTEM_TYPE.DARWIN) {
+                            versionToDownload = "1135562";
+                        }
+                        else if (plateform.getSystem() == plateform_1.SYSTEM_TYPE.LINUX) {
+                            versionToDownload = "1135561";
+                        }
                     }
                     else {
                         throw "Invalid chrome version (112 or 113 or 114 version accepted)";
--- .github/actions/setup-chrome/src/index.ts
+++ .github/actions/setup-chrome/src/index.ts
@@ -1,18 +1,21 @@
 import * as core from "@actions/core";
 import { Install } from "./install";
-import { Plateform } from "./plateform";
+import { Plateform, SYSTEM_TYPE } from "./plateform";
 
 /*
 https://www.chromium.org/getting-involved/download-chromium/
 https://chromiumdash.appspot.com/branches
 http://cros-omahaproxy.appspot.com/
 
-112 => 1109224 => 1109220
+112 => 1109224 => 1109220 (Unix), 1109213 (Mac)
 https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Linux_x64/1109220/
-113 => 1121455 => 1121454
+https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Mac/1109213/
+113 => 1121455 => 1121454 (Unix), 1121458 (Mac)
 https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Linux_x64/1121454/
-114 => 1135570 => 1135561
+https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Mac/1121448/
+114 => 1135570 => 1135561 (Unix), 1135562 (Mac)
 https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Linux_x64/1135561/
+https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Mac/1135562/
 */
 
 class Index {
@@ -31,11 +34,23 @@ class Index {
                 // Identify to download
                 let versionToDownload = "";
                 if ( version == "112") {
-                    versionToDownload = "1109220";
+                    if (plateform.getSystem() == SYSTEM_TYPE.DARWIN) {
+                        versionToDownload = "1109213";
+                    } else if (plateform.getSystem() == SYSTEM_TYPE.LINUX) {
+                        versionToDownload = "1109220";
+                    }
                 } else if ( version == "113" ) {
-                    versionToDownload = "1121454";
+                    if (plateform.getSystem() == SYSTEM_TYPE.DARWIN) {
+                        versionToDownload = "1121458";
+                    } else if (plateform.getSystem() == SYSTEM_TYPE.LINUX) {
+                        versionToDownload = "1121454";
+                    }
                 } else if ( version == "114" ) {
-                    versionToDownload = "1135561";
+                    if (plateform.getSystem() == SYSTEM_TYPE.DARWIN) {
+                        versionToDownload = "1135562";
+                    } else if (plateform.getSystem() == SYSTEM_TYPE.LINUX) {
+                        versionToDownload = "1135561";
+                    }
                 } else {
                     throw "Invalid chrome version (112 or 113 or 114 version accepted)";
                 }
